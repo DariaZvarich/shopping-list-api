b9a0ea226dbb5ecb41bd5a8b3bda6a54c749f86d
Daria Zvarich <your email>
2023-10-28 17:47:02 +0100

    Added delete, add and toggle

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const ShoppingList = () => {
     const [items, setItems] = useState(null);
+    const [name, setName] = useState('');
 
     useEffect(() => {
         axios.get('http://localhost:3001/api/items')
@@ -19,16 +20,43 @@ const ShoppingList = () => {
             <div>
                 You have no items
             </div>
-        )
+        );
+    }
+
+    function handleRemove(id){
+        const deleteItem = items.filter((item) => item.id !== id);
+        setItems(deleteItem);
+    }
+    function handleAddButtonClick(id){
+        const newItem = {
+            id: items.length + 1,
+            name: name,
+            quantity: 1,
+            isSelected: false,
+        };
+        setItems([...items, newItem]);
+        setName('');
     }
 
     return (
         <div>
+            <input type = "text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Enter Item name' />
+            <button onClick={handleAddButtonClick}> Add Item </button>
+            <ul>
             {items.map((item) => (
-                <div key={item.id}>{item.name}</div>
-            ))}    
+                <li key={item.id}>
+                <span>{item.name}</span>
+                <button type="button" onClick={() => handleRemove(item.id)}>
+                    Remove
+                </button>
+                <button>
+                    Add Item
+                </button>
+                </li>
+            ))}
+            </ul>
         </div>
-    );
+      );
 };
 
 export default ShoppingList;
